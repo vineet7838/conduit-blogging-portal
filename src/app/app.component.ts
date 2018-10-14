f8e084d6b244f3b09950c37bb395362ea10532e9
vineet7838 <vineetagarwal267@gmail.com>
2018-10-14 19:33:43 +0530

    Added display service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LoginService} from './login.service';
+
 import { NgForm } from '@angular/forms';
 import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
 
@@ -11,21 +11,6 @@ import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_di
 })
 export class AppComponent {
   title = 'Login-testing';
-  public selected: Object;
-  constructor(private getData: LoginService){
- 
-    }
-    registerUser(form: NgForm){
-     
-     this.getData.authUser(form.value).subscribe((status: Object )=> {
-      this.displayData(status);
-    });
-    
-    }
-    displayData(data){
-      this.selected = data;
-      console.log(this.selected);
-    }
-    
+  constructor(){}
   ngOnInit(){}
 }
